ffcce02c0d7a2ef7946e1730707808a450ab45e5
drmajstorovic <draganamajstorovic02@yahoo.com>
2024-04-11 16:03:12 +0200

    fixed the combat trigger issue B)

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,17 @@
-class Position(var x: Int, var y: Int)
+class Position(var x: Int, var y: Int){
+    override fun equals(other: Any?): Boolean {
+        if(other is Position)
+            if((this.x == other.x) and (this.y == other.y))
+                return true
+        return false
+    }
+
+    override fun hashCode(): Int {
+        var result = x
+        result = 31 * result + y
+        return result
+    }
+}
 var fighting = false
 fun main() {
     print("Enter your name: ")
